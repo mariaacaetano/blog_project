f54bdd24f21a0032d5739769cc8e12c27a6deb08
Maria Fernanda Caetano <nanda2001caetano@gmail.com>
2024-11-04 19:59:08 -0300

    deleted migrations to reset database (tests)

--- posts_app/views.py
+++ posts_app/views.py
@@ -5,15 +5,18 @@ from django.contrib import messages
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.shortcuts import redirect
 from django.views import generic
-from .models import Posts
-from .forms import PostsForm
+from .models import Posts, Comments
+from .forms import PostsForm, UserProfileForm, CommentsForm
 
 
 # Create your views here.
 def post_list(request):
-    template_name = 'post-list.html' # template
+    template_name = 'post_list.html' # template
     posts = Posts.objects.all() # query com todas as postagens
     context = { # cria context para chamar no template
         'posts': posts
@@ -28,57 +31,115 @@ def educacao_list(request):
     }
     return render(request, template_name, context)  # Renderiza a página com o contexto
 
-
+@login_required
 def post_create(request):
-    if request.method == 'POST': # para metodo POST
-        form = PostsForm(request.POST, request.FILES) # pega as informações do form
-        if form.is_valid(): # se for valido
-            form = form.save(commit=False)
-            form.save() # salva
-            
-            messages.success(request, 'O post foi criado com sucesso') # mensagem quando cria o post
-            return HttpResponseRedirect(reverse('post-list')) # coloquei para retornar post-list
+    form = PostsForm(request.POST or None, request.FILES or None)  # Não é necessário passar 'author' aqui
+
+    if form.is_valid():
         
-    form = PostsForm() # senão carrega o formulario  
-    return render(request, 'post-form.html', {"form": form}) # nesse template
+        post = form.save(commit=False)  # Não salva o post imediatamente
+        post.author = request.user # Define o autor como o usuário logado
+        post.save()  # Salva o post
+        messages.success(request, 'O post foi criado com sucesso.')
+        return HttpResponseRedirect(reverse('post_list'))  # Redireciona para a lista de posts
+
+    return render(request, 'post_form.html', {"form": form})  # Renderiza o formulário se não for um POST válido
 
 
 def post_detail(request, id):
-    template_name = 'post-detail.html' # template
-    post = Posts.objects.get(id=id) # Metodo Get
-    context = { # cria context para chamar no template
-        'post': post
-        }
-    return render(request, template_name, context) # render
+    post = get_object_or_404(Posts, id=id)  # Obtém o post
+    comments = post.comments_set.all()  # Obtém todos os comentários associados ao post
 
-
-def post_update(request, id):
-    post = get_object_or_404(Posts, id=id)  # Obtém o post com o ID fornecido
-    form = PostsForm(request.POST or None, request.FILES or None, instance=post)  # Preenche o formulário com os dados do post
-
-    if form.is_valid():  # Verifica se o formulário é válido
-        form.save()  # Salva as alterações no post
-        
-        messages.success(request, 'O post foi atualizado com sucesso')  # Mensagem de sucesso
-        return HttpResponseRedirect(reverse('post-detail', args=[post.id]))  # Redireciona para a página de detalhes do post
-        
-    return render(request, 'post-form.html', {"form": form, "post": post})  # Renderiza o formulário se não for um POST válido
-
-
-def post_delete(request, id):
-    post = get_object_or_404(Posts, id=id)  # Garante que retorna 404 se não existir
     if request.method == 'POST':
-        # Verifica se há uma imagem associada ao post e a remove
+        comment_form = CommentsForm(request.POST)  # Cria o formulário com os dados do POST
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)  # Não salva imediatamente
+            comment.author = request.user  # Define o autor como o usuário logado
+            comment.post = post  # Associa o comentário ao post
+            comment.save()  # Salva o comentário
+            return redirect('post_detail', id=post.id)  # Redireciona para a página do post
+
+    else:
+        comment_form = CommentsForm()  # Cria um novo formulário vazio
+
+    return render(request, 'post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form
+    })
+
+
+@login_required
+def post_update(request, id):
+    post = get_object_or_404(Posts, id=id)
+    
+    # Verifica se o usuário logado é o autor do post
+    if post.author != request.user:
+        messages.error(request, 'Você não tem permissão para editar este post.')
+        return redirect('post_list')  # Redireciona para a lista de posts, ou outra página
+
+    form = PostsForm(request.POST or None, request.FILES or None, instance=post)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'O post foi atualizado com sucesso')
+        return HttpResponseRedirect(reverse('post_detail', args=[post.id]))
+    
+    return render(request, 'post_form.html', {"form": form, "post": post})
+
+@login_required
+def comment_delete(request, id):
+    comment = get_object_or_404(Comments, id=id)
+
+    # Verifica se o usuário é o autor do comentário ou um admin
+    if request.user == comment.author or request.user.is_staff and request.method == "POST":
+        comment.delete()
+        messages.success(request, 'Comentário excluído com sucesso.')
+    else:
+        messages.error(request, 'Você não tem permissão para excluir este comentário.')
+
+    return redirect('post_detail', id=comment.post.id)  # Redireciona para a página do post
+
+@login_required
+def post_delete(request, id):
+    post = get_object_or_404(Posts, id=id)
+
+    # Verifica se o usuário logado é o autor do post ou um superadmin
+    if post.author != request.user and not request.user.is_staff:
+        messages.error(request, 'Você não tem permissão para deletar este post.')
+        return redirect('post_list')  # Redireciona para a lista de posts, ou outra página
+
+    if request.method == 'POST':
         if post.image and os.path.isfile(os.path.join(settings.MEDIA_ROOT, post.image.path)):
             os.remove(os.path.join(settings.MEDIA_ROOT, post.image.path))
         
-        post.delete()  # Deleta o post do banco de dados
-        messages.error(request, 'O post foi deletado com sucesso')
-        return HttpResponseRedirect(reverse('post-list'))  # Redireciona para a lista de posts
+        post.delete()
+        messages.success(request, 'O post foi deletado com sucesso.')
+        return HttpResponseRedirect(reverse('post_list'))
     
-    return render(request, 'post-delete.html', {'post': post})  # Renderiza a página de confirmação
+    return render(request, 'post_delete.html', {'post': post})
 
-class SignUpView(generic.CreateView): 
-   form_class = UserCreationForm
-   success_url = reverse_lazy('login') 
-   template_name = 'registration/signup.html'
+
+def complete_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')  # Redirecione para a página inicial ou onde desejar
+    else:
+        form = UserProfileForm(instance=request.user)
+    return render(request, 'registration/complete_profile.html', {'form': form})
+
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login') 
+    
+    def form_valid(self, form):
+        # Salva o novo usuário
+        user = form.save()
+        # Loga o usuário automaticamente após o signup
+        login(self.request, user)
+        # Redireciona para a página de completar perfil
+        return redirect('complete_profile')
